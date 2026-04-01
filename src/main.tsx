710081e2f1230f03d75975b46d7119859aad54c7
nnadya <n.d.niki2012@gmail.com>
2026-04-01 22:50:43 +0300

    add QueryProvider and AppRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+//import './index.css'
+import './styles/index.scss'
+//import App from './App.tsx'
+
+import { QueryProvider } from './app/providers/query-provider.tsx'
+import { AppRouter } from './app/providers/router-provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <QueryProvider>
+      <AppRouter />
+    </QueryProvider>
   </StrictMode>,
 )
