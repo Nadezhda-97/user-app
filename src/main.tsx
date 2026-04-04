95c4813cda449762dfd54dd009038fcb6d612dc6
nnadya <n.d.niki2012@gmail.com>
2026-04-04 18:59:07 +0300

    delete comments

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import './index.css'
-import './styles/index.scss'
-//import App from './App.tsx'
 
 import { QueryProvider } from './app/providers/query-provider.tsx'
 import { AppRouter } from './app/providers/router-provider.tsx'
 
+import './styles/index.scss'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
     <QueryProvider>
       <AppRouter />
     </QueryProvider>
