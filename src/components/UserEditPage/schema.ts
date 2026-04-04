b13a4494204d305923ddee9c9e1371c85bd33768
nnadya <n.d.niki2012@gmail.com>
2026-04-04 18:43:56 +0300

    fix text

--- src/components/UserEditPage/schema.ts
+++ src/components/UserEditPage/schema.ts
@@ -2,11 +2,11 @@ import { z } from 'zod';
 
 export const schema = z.object({
   name: z.string().min(2, 'Необходимо минимум 2 символа').max(64, 'Максимум 64 символа'),
-  username: z.string().min(2, 'Необходимо минимум 2 символа').max(64, 'Максимум 64 символов'),
+  username: z.string().min(2, 'Необходимо минимум 2 символа').max(64, 'Максимум 64 символа'),
   email: z.email('Некорректный формат email'),
-  city: z.string().min(2, 'Необходимо минимум 2 символа').max(64, 'Максимум 64 символов'),
+  city: z.string().min(2, 'Необходимо минимум 2 символа').max(64, 'Максимум 64 символа'),
   phone: z.string().regex(/^\d+$/, 'Только цифры'),
-  company: z.string().min(2, 'Необходимо минимум 2 символа').max(64, 'Максимум 64 символов'),
+  company: z.string().min(2, 'Необходимо минимум 2 символа').max(64, 'Максимум 64 символа'),
 });
 
 export type FormData = z.infer<typeof schema>;
